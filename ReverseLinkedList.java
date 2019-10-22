3607f6b3fe1db0186dba653bad714fe3d2fc15fa
powerfulTaxi <321ijile@gmail.com>
2019-10-22 10:57:47 -0400

    reverse in progres...

---
+++ ReverseLinkedList.java
@@ -0,0 +1,28 @@
+/* *****************************************************************************
+ *  Name:    Eli Ji
+ *  Date: 10-15-19
+ *  Description: This class reverses a linked list.
+ **************************************************************************** */
+
+import java.util.LinkedList;
+
+public class ReverseLinkedList {
+    public static void main(String[] args) {
+        LinkedList myList = new LinkedList();
+        myList.add(1);
+        myList.add(2);
+        myList.add(3);
+        myList.add(4);
+        myList.add(5);
+        System.out.println(reverseList(myList));
+    }
+
+    public static LinkedList reverseList(LinkedList l){
+        for(Object i : l){
+
+        }
+
+
+        return l;
+    }
+}
